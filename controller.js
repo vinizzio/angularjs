03a7752f8102e5dad0199b419b2771c96f00cc18
vinizzio <vinizzio@hotmail.com>
2017-02-11 16:18:44 -0500

    1ra. peticion

--- controller.js
+++ controller.js
@@ -1,8 +1,10 @@
 angular.module("MyFirstApp",[])
   .controller("FirstController",function($scope,$http){
+    $scope.posts = [];
     $http.get("http://jsonplaceholder.typicode.com/posts")
     .success(function(data){
         console.log(data);
+        $scope.posts = data;
     })
     .error(function(err){
 
