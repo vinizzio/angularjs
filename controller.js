fb641472ea0f44b35f073fecf68bca46cd1851bb
vinizzio <vinizzio@hotmail.com>
2017-02-11 18:33:05 -0500

    agregar nuevo post

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 angular.module("MyFirstApp",[])
   .controller("FirstController",function($scope,$http){
     $scope.posts = [];
+    $scope.newPost = {};
     $http.get("http://jsonplaceholder.typicode.com/posts")
     .success(function(data){
         console.log(data);
@@ -9,4 +10,20 @@ angular.module("MyFirstApp",[])
     .error(function(err){
 
     });
+    $scope.addPost = function() {
+      $http.post("http://jsonplaceholder.typicode.com/posts",{
+        title: $scope.newPost.title,
+        body: $scope.newPost.body,
+        userId: 1
+      })
+      .success(function(data,status,headers,config) {
+          console.log(data);
+          $scope.posts.push($scope.newPost);
+          $scope.newPost = {};
+
+      })
+      .error(function(error,status,headers,config){;
+        console.log(error);
+    });
+  }
   });
